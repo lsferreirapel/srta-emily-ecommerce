44ffbe60117f157b75b6cc5cf6ea5098feb8492c
Lucas Ferreira <lsferreirapel@gmail.com>
2020-11-17 19:03:03 -0300

    :wrench: Estrutura padrão de arquivos
     **Configurações padrão:**
      - Adicionado estrutura de rotas
      - Adicionado estilo global
    
    Pagina "Home" criada para teste, exibe a mensagem "Hello World!".

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,22 @@
 /* React imports */
 import React from 'react';
 
-const App: React.FC = () => <div className="App" />;
+/* APIs import */
+import { BrowserRouter } from 'react-router-dom';
+
+/* My routes */
+import Routes from './routes';
+
+/* Global style */
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
