2e914f59c746e184da9c5b20f62a15ac5642db40
Lucas Ferreira <lsferreirapel@gmail.com>
2020-12-08 11:43:26 -0300

    :art: Adiciona componente UI "NewLabel"

---
+++ web/src/components/UI/NewLabel/index.tsx
@@ -0,0 +1,13 @@
+/* React imports */
+import React from 'react';
+
+/* Styles */
+import Container from './styles';
+
+const NewLabel: React.FC = () => (
+  <Container>
+    <h2>NEW</h2>
+  </Container>
+);
+
+export default NewLabel;
